8ac01523e5a9f47f3c3c6c697d5958e32555d5ba
Shahriar Kabir <shahriarkabir.aero@gmail.com>
2024-08-17 23:23:38 +0200

    Deprecation error from punycode is gone

---
+++ src/lucide-react.d.ts
@@ -0,0 +1,8 @@
+declare module 'lucide-react' {
+  import React from 'react';
+  export const ChevronLeft: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const ChevronRight: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const Maximize: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const Minimize: React.FC<React.SVGProps<SVGSVGElement>>;
+  // Add any other icons you're using from lucide-react
+}
